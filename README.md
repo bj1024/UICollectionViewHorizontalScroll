2d7ed811b1e42bfd3bfb0a5cd6628dd396f4bde5
bj1024 <16552133+bj1024@users.noreply.github.com>
2019-08-13 16:36:39 +0900

    update Readme.

--- README.md
+++ README.md
@@ -8,3 +8,49 @@
 [Customizing Collection View Layouts \| Apple Developer Documentation](https://developer.apple.com/documentation/uikit/uicollectionview/customizing_collection_view_layouts)
 
 
+# how to 
+
+## custom class of UICollectionViewFlowLayout
+
+Create custom class of UICollectionViewFlowLayout and override prepare.
+Set the following properties in the prepar function.
+
+```
+    open var scrollDirection: UICollectionView.ScrollDirection // default is UICollectionViewScrollDirectionVertical
+
+    open var minimumLineSpacing: CGFloat
+
+    open var minimumInteritemSpacing: CGFloat
+
+    open var itemSize: CGSize
+
+```
+
+## example
+
+```Swift
+ override func prepare() {
+    super.prepare()
+
+    guard let collectionView = collectionView else { return }
+    itemSize = CGSize(width: collectionView.bounds.width * 0.8, // 80% width
+                      height: collectionView.bounds.height)
+    minimumLineSpacing = 10 // space between items
+    minimumInteritemSpacing = (collectionView.bounds.width - itemSize.width) / 2.0 // 1st or last item margin
+
+    sectionInset = UIEdgeInsets(top: 0.0,
+                                left: minimumInteritemSpacing,
+                                bottom: 0.0,
+                                right: minimumInteritemSpacing)
+    sectionInsetReference = .fromContentInset
+    scrollDirection = .horizontal
+  }
+```
+
+
+## CollectionView storyboard property
+
+Layout : Custom
+class  : UICollectionViewHorizontalFlowLayout
+
+## TBD
--- UICollectionViewHorizontalScroll/ViewController.swift
+++ UICollectionViewHorizontalScroll/ViewController.swift
@@ -8,9 +8,9 @@ class ViewController: UIViewController {
   @IBOutlet weak var collectionView: UICollectionView!
 
   let images: [UIImage] = [
-//    UIImage(named: "image00")!,
-//    UIImage(named: "image01")!,
-//    UIImage(named: "image02")!,
+    UIImage(named: "image00")!,
+    UIImage(named: "image01")!,
+    UIImage(named: "image02")!,
     UIImage(named: "image03")!,
     UIImage(named: "image04")!,
     UIImage(named: "image05")!
@@ -25,45 +25,16 @@ class ViewController: UIViewController {
 }
 
 extension ViewController: UICollectionViewDelegate {
-//  func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-//    guard let cell = self.collectionView.cellForItem(at: indexPath ) as? AlbumCollectionViewCell else { return }
-//    let selectedCollection: [PHAssetCollection] = [photoCollections[indexPath.row]]
-//
-//    cell.transform = CGAffineTransform(scaleX: 1.1, y: 1.1)
-//    UIView.animate(withDuration: 0.3,
-//                   delay: 0,
-//                   options: .curveEaseOut,
-//                   animations: {
-//                    cell.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
-//    },
-//                   completion: { _ in
-//                    self.completion?(.success(selectedCollection))
-//                    self.dismiss(animated: true, completion: nil)
-//    })
-//  }
+
+  func scrollViewDidScroll(_ scrollView: UIScrollView) {
+    print("scrollViewDidScroll \(collectionView.position)")
+
+  }
+  func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+    print("willDisplay \(indexPath)")
+  }
 }
 
-// extension ViewController: UICollectionViewDelegateFlowLayout {
-//  func collectionView(_ collectionView: UICollectionView,
-//                      layout collectionViewLayout: UICollectionViewLayout,
-//                      sizeForItemAt indexPath: IndexPath) -> CGSize {
-//    guard let flowLayout = collectionViewLayout as? UICollectionViewFlowLayout else { abort() }
-////    let sectionInset = flowLayout.sectionInset
-////
-////    let column = CGFloat(columnNum)
-////    let contentW: CGFloat = collectionView.bounds.width
-////      - sectionInset.left
-////      - sectionInset.right
-////      - collectionView.contentInset.left
-////      - collectionView.contentInset.right
-////      - flowLayout.minimumInteritemSpacing * (column - 1)
-////
-////    let itemW: CGFloat = contentW / column
-////    let itemH: CGFloat = itemW + 22 * 2 // 2 label height
-////    return CGSize(width: itemW, height: itemH)
-//    return CGSize(width: 100 , height: 100)
-//  }
-// }
 
 extension ViewController: UICollectionViewDataSource {
   func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
