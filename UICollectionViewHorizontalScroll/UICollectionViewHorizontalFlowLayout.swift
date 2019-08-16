6b463dd6f08088a2c3f130e02cfa356fe52ff2b2
bj1024 <16552133+bj1024@users.noreply.github.com>
2019-08-16 12:56:58 +0900

    scroll to page if velocity is present.

--- UICollectionViewHorizontalScroll/UICollectionViewHorizontalFlowLayout.swift
+++ UICollectionViewHorizontalScroll/UICollectionViewHorizontalFlowLayout.swift
@@ -37,26 +37,51 @@ class UICollectionViewHorizontalFlowLayout: UICollectionViewFlowLayout {
     guard let collectionView = collectionView else {
       return super.targetContentOffset(forProposedContentOffset: proposedContentOffset, withScrollingVelocity: velocity)
     }
-    print("forProposedContentOffset \(proposedContentOffset) \(velocity)")
+    // proposedContentOffset is scrollOffset suggestion calcurated by base class.
+    print("targetContentOffset \(collectionView.contentOffset) -> \(proposedContentOffset) velocity=\(velocity)")
 
-    var offsetAdjustment = CGFloat.greatestFiniteMagnitude
-    let horizontalOffset = proposedContentOffset.x + collectionView.contentInset.left
-
-    let targetRect = CGRect(x: proposedContentOffset.x, y: 0, width: collectionView.bounds.size.width, height: collectionView.bounds.size.height)
-
-    let layoutAttributesArray = super.layoutAttributesForElements(in: targetRect)
-
-    layoutAttributesArray?.forEach { layoutAttributes in
-      let itemOffset = layoutAttributes.frame.origin.x
-      print("itemOffset \(layoutAttributes.frame)")
-
-      if fabsf(Float(itemOffset - horizontalOffset)) < fabsf(Float(offsetAdjustment)) {
-        offsetAdjustment = itemOffset - horizontalOffset - (collectionView.frame.width - layoutAttributes.frame.width) / 2.0
+    // add contentInset
+    var searchPosX = proposedContentOffset.x + collectionView.contentInset.left
+    // If velocity is present, shift the search position to page up.
+    if abs(velocity.x) > 0 {
+      if velocity.x > 0 {
+        searchPosX += itemSize.width / 2 // right page
+      }
+      else {
+        searchPosX -= itemSize.width / 2 // left page
       }
     }
 
-    let pt = CGPoint(x: proposedContentOffset.x + offsetAdjustment, y: proposedContentOffset.y)
-    print("forProposedContentOffset \(proposedContentOffset) \(velocity) ret=\(pt)")
+    // Search items rect in current scrolling offset rect
+    let searchRect = CGRect(x: proposedContentOffset.x,
+                            y: 0,
+                            width: collectionView.bounds.size.width,
+                            height: collectionView.bounds.size.height)
+
+    guard let itemsInBounds = super.layoutAttributesForElements(in: searchRect) else {
+      return super.targetContentOffset(forProposedContentOffset: proposedContentOffset, withScrollingVelocity: velocity)
+    }
+
+    // search nearest page layout
+    var offsetX: CGFloat
+    let nearestItem = itemsInBounds.min { a, b in fabsf(Float(a.frame.origin.x - searchPosX)) < fabsf(Float(b.frame.origin.x - searchPosX)) }
+    if let nearestItem = nearestItem {
+      print("nearestItem=\(nearestItem.indexPath) x=\(nearestItem.frame.origin.x))")
+      offsetX = nearestItem.frame.origin.x - (collectionView.frame.width - nearestItem.frame.width) / 2.0
+    }
+    else {
+      print("nearestItem=nil")
+      offsetX = searchPosX
+    }
+
+    let pt = CGPoint(x: offsetX, y: proposedContentOffset.y)
+    print("forProposedContentOffset contentOffset=\(collectionView.contentOffset) proposed=\(proposedContentOffset) velocity=\(velocity) ret=\(pt)")
     return pt
+  }
+
+  @objc
+  func injected() {
+    print("I've been injected: \(self)")
+    //    autoScroll()
   }
 }
